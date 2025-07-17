b938518bf8f325b0bcbad673cb209fcaf892b278
Matin Ansaripour <matin.ansari.pour.84@gmail.com>
2025-07-17 09:53:45 +0330

    feat(Layout): header and footer fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
-
+import React from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import HomePage from "./Pages/HomePage.jsx";
+import About from "./Pages/About.jsx";
+import Cart from "./Pages/Cart.jsx";
+import ProductDetails from "./Pages/ProductDetails.jsx";
+import Products from "./Pages/Products.jsx";
+import Layout from "./Layout/Layout.jsx";
 function App() {
   return (
-    <div className="bg-red-500 text-white p-4">
-  Hello Tailwind!
-</div>
-  )
+    
+      <BrowserRouter>
+       <Layout> <Routes>
+          <Route path="/" element={<Navigate to="/Homepage" replace />} />
+          <Route path="/Homepage" element={<HomePage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+        </Routes></Layout>
+      </BrowserRouter>
+    
+  );
 }
 
-export default App
+export default App;
